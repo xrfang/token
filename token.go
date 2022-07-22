7e54d5cfd4c59427a3a6d3ef0683aac5585d5f4c
xrfang <xrfang@gmail.com>
2022-07-22 22:34:25 +0800

    removed source IP checking

--- token.go
+++ token.go
@@ -8,8 +8,6 @@ import (
 	"encoding/binary"
 	"encoding/hex"
 	"errors"
-	"fmt"
-	"net"
 	"time"
 )
 
@@ -19,7 +17,7 @@ func assert(e interface{}) {
 	}
 }
 
-func New(ident uint64, expire time.Time, owner net.IP) string {
+func New(ident uint64, expire time.Time) string {
 	raw := []byte{0}
 	rand.Read(raw)
 	buf := make([]byte, 8)
@@ -28,9 +26,6 @@ func New(ident uint64, expire time.Time, owner net.IP) string {
 	binary.BigEndian.PutUint64(buf, ident)
 	raw = append(raw, buf...)
 	raw = append(raw, raw[0])
-	if owner != nil {
-		raw = append(raw, owner.To16()...)
-	}
 	block, _ := aes.NewCipher(tokenKey)
 	iv := bytes.Repeat([]byte{0}, aes.BlockSize)
 	cbc := cipher.NewCBCEncrypter(block, iv)
@@ -38,7 +33,7 @@ func New(ident uint64, expire time.Time, owner net.IP) string {
 	return hex.EncodeToString(raw)
 }
 
-func Verify(token string, holder net.IP) (ident uint64, err error) {
+func Verify(token string) (ident uint64, err error) {
 	defer func() {
 		if e := recover(); e != nil {
 			err = errors.New("corrupted token")
@@ -50,22 +45,8 @@ func Verify(token string, holder net.IP) (ident uint64, err error) {
 	iv := bytes.Repeat([]byte{0}, aes.BlockSize)
 	cbc := cipher.NewCBCDecrypter(block, iv)
 	cbc.CryptBlocks(data, data)
-	var owner net.IP
-	switch len(data) {
-	case 16:
-	case 32:
-		owner = net.IP(data[16:])
-		data = data[:16]
-	default:
-		panic(fmt.Errorf("invalid data length (%d), expect 16 or 32", len(data)))
-	}
 	if data[0] != data[len(data)-1] {
 		panic(errors.New("invalid head/tail"))
-	}
-	if len(owner) > 0 {
-		if !bytes.Equal(owner, holder) {
-			return 0, errors.New("illegal token")
-		}
 	}
 	timestamp := binary.BigEndian.Uint64(append([]byte{0, 0}, data[1:7]...))
 	exp := time.Unix(int64(timestamp), 0)
--- token_test.go
+++ token_test.go
@@ -1,16 +1,15 @@
 package token
 
 import (
-	"net"
 	"testing"
 	"time"
 )
 
 func TestValidToken(t *testing.T) {
 	uid := uint64(123456)
-	tok := New(uid, time.Now().Add(time.Minute), nil)
+	tok := New(uid, time.Now().Add(time.Minute))
 	t.Logf("generated token for uid#%d: %s", uid, tok)
-	ident, err := Verify(tok, nil)
+	ident, err := Verify(tok)
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
@@ -20,26 +19,13 @@ func TestValidToken(t *testing.T) {
 func TestExpiredToken(t *testing.T) {
 	uid := uint64(123456)
 	exp := time.Now()
-	tok := New(uid, exp, nil)
+	tok := New(uid, exp)
 	t.Logf("generated token for uid#%d: %s", uid, tok)
 	t.Logf("token will expire after %s", exp)
-	_, err := Verify(tok, nil)
+	_, err := Verify(tok)
 	now := time.Now()
 	if err == nil {
 		t.Fatalf("token is still valid at %s", now)
 	}
 	t.Logf("token is expired at %s (err=%v)", now, err)
-}
-
-func TestIllegalToken(t *testing.T) {
-	uid := uint64(123456)
-	owner := net.IPv4(127, 0, 0, 1)
-	tok := New(uid, time.Now().Add(time.Minute), owner)
-	t.Logf("generated token for uid#%d@%s: %s", uid, owner, tok)
-	holder := net.IPv4(192, 168, 1, 1)
-	_, err := Verify(tok, holder)
-	if err == nil {
-		t.Fatalf("token with owner %q is valid for %q", owner, holder)
-	}
-	t.Logf("token owned by %q not usable by %q: %v", owner, holder, err)
 }
